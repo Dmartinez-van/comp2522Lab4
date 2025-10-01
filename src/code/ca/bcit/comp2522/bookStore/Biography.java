88d5aae7b15071fcfefefb08e7495aeb6e4fe25e
David Martinez <Dmartinez21@my.bcit.ca>
2025-10-01 07:22:29 -0700

    Add javadoc template to all files

--- src/code/ca/bcit/comp2522/bookStore/Biography.java
+++ src/code/ca/bcit/comp2522/bookStore/Biography.java
@@ -1,5 +1,13 @@
 package ca.bcit.comp2522.bookStore;
 
+//TODO ADD JAVADOC COMMENT
+/**
+ * <>ADD JAVADOC COMMENT</>
+ *
+ * @author David Martinez,
+ *         Daniel Do
+ * @version 1.0
+ */
 public class Biography
 {
 }
--- src/code/ca/bcit/comp2522/bookStore/Name.java
+++ src/code/ca/bcit/comp2522/bookStore/Name.java
@@ -1,5 +1,13 @@
 package ca.bcit.comp2522.bookStore;
 
+/**
+ * Name class with a First and Last name.
+ * Implements Printable
+ *
+ * @author David Martinez,
+ *         Daniel Do
+ * @version 1.0
+ */
 public class Name
 {
 }
