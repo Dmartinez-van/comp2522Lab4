872a513bf2a16139ad09d674d5de45d78e91fd4f
David Martinez <Dmartinez21@my.bcit.ca>
2025-10-01 07:18:11 -0700

    setting up all classes and interfaces files

---
+++ src/code/Printable.java
@@ -0,0 +1,17 @@
+/**
+ * Printable interface (SAM)
+ *
+ * @author David Martinez,
+ *         Daniel Do
+ * @version 1.0
+ */
+public interface Printable
+{
+    /**
+     * Prints every instance variable (including those in parent classes)
+     * in a sentence.
+     *
+     * @return a string sentence
+     */
+    String display();
+}
---
+++ src/code/Reversible.java
@@ -0,0 +1,16 @@
+/**
+ * Reverse interface (SAM)
+ *
+ * @author David Martinez,
+ *         Daniel Do
+ * @version 1.0
+ */
+public interface Reversible
+{
+    /**
+     * Prints a given string in reverse.
+     *
+     * @return a reversed string
+     */
+    String backward();
+}
