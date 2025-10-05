31eca8ac3ccfcffc085c7560bc478aa79e7ec921
Daniel Do <dodaniel006@gmail.com>
2025-10-04 21:12:19 -0700

    Implement the Author class

--- src/code/ca/bcit/comp2522/bookStore/Author.java
+++ src/code/ca/bcit/comp2522/bookStore/Author.java
@@ -8,6 +8,57 @@ package ca.bcit.comp2522.bookStore;
  *         Daniel Do
  * @version 1.0
  */
-public class Author
+public class Author extends Person
+                    implements Printable
 {
+    private static final int NAME_CHAR_LIMIT = 30;
+
+    private final String genre;
+
+    public Author(final Date birthDate,
+                  final Date deathDate,
+                  final Name name,
+                  final String genre)
+    {
+        super (birthDate, deathDate, name);
+
+        checkGenreString(genre);
+        this.genre = genre;
+    }
+
+    public Author (final Date birthDate,
+                   final Name name,
+                   final String genre)
+    {
+        super (birthDate, name);
+        this.genre = genre;
+    }
+
+    private void checkGenreString(final String genre)
+    {
+        if(genre == null || genre.isBlank())
+        {
+            throw new IllegalArgumentException("Genre cannot be null or blank");
+        }
+        if (genre.length() > NAME_CHAR_LIMIT)
+        {
+            throw new IllegalArgumentException("Genre cannot be longer than " +
+                                                NAME_CHAR_LIMIT);
+        }
+    }
+
+    @Override
+    public void display()
+    {
+        super.display();
+
+        final StringBuilder sb;
+        sb = new StringBuilder();
+
+        sb.append("Genre: ");
+        sb.append(genre);
+
+        System.out.println(sb.toString());
+    }
+
 }
